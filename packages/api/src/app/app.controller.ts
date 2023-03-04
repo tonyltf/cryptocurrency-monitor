3fcb9b5c40398f1ba4cae539dc45df73905529e3
Tony Li <tingfung.tony@gmail.com>
2023-03-05 03:39:26 +0800

    refactor(app): Switch to nx monorepo

---
+++ packages/api/src/app/app.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
+import { AppService } from './app.service';
+import { TickerService } from '../ticker/ticker.service';
+import { ITickerResult } from '../ticker/ticker.inteface';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly tickerService: TickerService,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/list')
+  getCurrencyList(): { [key: string]: string } {
+    return this.tickerService.getCurrencyList();
+  }
+
+  @ApiQuery({ name: 'pairs', isArray: true })
+  @Get('/price')
+  async getPrices(@Query('pairs') pairs: string | string[]): Promise<ITickerResult[]> {
+    if (typeof pairs === 'string') {
+      return this.tickerService.getPrices([pairs]);
+    } else {
+      return this.tickerService.getPrices(pairs);
+    }
+  }
+}
---
+++ packages/api/src/ticker/ticker.e2e.spec.ts
@@ -0,0 +1,58 @@
+import request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '../lib/cache/cache.module';
+import { LoggerModule } from '../lib/logger/logger.module';
+import { TickerService } from './ticker.service';
+import { AppController } from '../app/app.controller';
+import { AppService } from '../app/app.service';
+
+describe('Ticker', () => {
+  let app: INestApplication;
+  const tickerService = {
+    getCurrencyList: () => ({
+      Bitcoin: 'BTCUSDT',
+      Ether: 'ETHUSDT',
+      Litecoin: 'LTCUSDT',
+      Monero: 'XMRUSDT',
+      Ripple: 'XRPUSDT',
+      Dogecoin: 'DOGEUSDT',
+      Dash: 'DASHUSDT',
+    }),
+  };
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), HttpModule, CacheModule, LoggerModule],
+      controllers: [AppController],
+      providers: [AppService, TickerService],
+    })
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it(`/GET list`, () => {
+    return request(app.getHttpServer())
+      .get('/list')
+      .expect(200)
+      .expect(tickerService.getCurrencyList());
+  });
+
+  it(`/GET price for BTC`, () => {
+    return request(app.getHttpServer()).get('/price?pairs=BTCUSDT').expect(200);
+  });
+
+  it(`/GET price for BTC and ETH`, () => {
+    return request(app.getHttpServer())
+      .get('/price?pairs=BTCUSDT&pairs=ETHUSDT')
+      .expect(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
