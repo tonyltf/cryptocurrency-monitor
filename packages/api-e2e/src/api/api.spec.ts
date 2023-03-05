01d293b3fa2394ed5d6988ed83f9e13b670711f4
Tony Li <tingfung.tony@gmail.com>
2023-03-06 00:31:10 +0800

    test(api): e2e test

--- packages/api-e2e/src/api/api.spec.ts
+++ packages/api-e2e/src/api/api.spec.ts
@@ -1,18 +1,42 @@
 import axios from 'axios';
 
 describe('GET', () => {
+
   it('should return a message', async () => {
     const res = await axios.get(`/`);
+    const { data, status } = res;
 
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual('Hello World!');
+    expect(status).toBe(200);
+    expect(data).toEqual('Hello World!');
   });
-  ``
+
   it('should return a list', async () => {
     const res = await axios.get(`/list`);
+    const { data, status } = res;
 
-    expect(res.status).toBe(200);
-    expect(res.data['Bitcoin']).toContain('BTCUSDT');
-    expect(res.data['Ether']).toContain('ETHUSDT');
+    expect(status).toBe(200);
+    expect(data?.['Bitcoin']).toContain('BTCUSDT');
+    expect(data?.['Ether']).toContain('ETHUSDT');
+  });
+
+  it('should return single price', async () => {
+    const res = await axios.get(`/price?pairs=BTCUSDT`);
+    const { data, status } = res;
+
+    expect(status).toBe(200);
+    expect(data).toHaveLength(1);
+    expect(data?.[0]?.symbol).toContain('BTCUSDT');
+    expect(data?.[0]?.price).toBeGreaterThan(0);
+  });
+
+  it('should return multiple price', async () => {
+    const res = await axios.get(`/price?pairs=BTCUSDT&pairs=ETHUSDT`);
+    const { data, status } = res;
+    expect(status).toBe(200);
+    expect(data).toHaveLength(2);
+    expect(data?.[0]?.symbol).toContain('BTCUSDT');
+    expect(data?.[0]?.price).toBeGreaterThan(0);
+    expect(data?.[1]?.symbol).toContain('ETHUSDT');
+    expect(data?.[1]?.price).toBeGreaterThan(0);
   });
 });
