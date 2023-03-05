5689b3dfe883a61e6563dbbdc963566fc31dccb3
Tony Li <tingfung.tony@gmail.com>
2023-03-05 17:54:28 +0800

    test(web): update e2e test

--- packages/web-e2e/src/e2e/app.cy.ts
+++ packages/web-e2e/src/e2e/app.cy.ts
@@ -8,6 +8,14 @@ describe('web', () => {
     // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getHeader().contains('Cryptocurrency Realtime pric');
+    getHeader().contains('Cryptocurrency Realtime price');
   });
+
+  it('should display cryptocurrency price list', () => {
+    cy.get("[data-testId=currencyList]")
+      .children()
+      .should("have.length.at.least", 1);
+    cy.get("[data-testId=currencyList]").should("contain.text", "Bitcoin")
+  });
+
 });
