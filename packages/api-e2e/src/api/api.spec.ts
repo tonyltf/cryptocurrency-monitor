95ba1c694144e4ca210f6bb7e0e853b73a2ded52
tony.li <tony@choco-up.com>
2023-03-05 16:54:51 +0800

    test(app): update e2e tests

--- packages/api-e2e/src/api/api.spec.ts
+++ packages/api-e2e/src/api/api.spec.ts
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
-describe('GET /', () => {
+describe('GET', () => {
   it('should return a message', async () => {
     const res = await axios.get(`/`);
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello World!' });
+    expect(res.data).toEqual('Hello World!');
+  });
+  ``
+  it('should return a list', async () => {
+    const res = await axios.get(`/list`);
+
+    expect(res.status).toBe(200);
+    expect(res.data['Bitcoin']).toContain('BTCUSDT');
+    expect(res.data['Ether']).toContain('ETHUSDT');
   });
 });
--- packages/web-e2e/src/e2e/app.cy.ts
+++ packages/web-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,13 @@
-import { getGreeting } from '../support/app.po';
+import { getHeader } from '../support/app.po';
 
 describe('web', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display app title`', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome web');
+    getHeader().contains('Cryptocurrency Realtime pric');
   });
 });
